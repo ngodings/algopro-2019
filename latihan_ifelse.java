5cbd9cf24220d70710ba32592a6f88e86f8e0309
ngodings <50858027+ngodings@users.noreply.github.com>
2019-10-16 09:46:16 +0700

    Create latihan_ifelse.java

---
+++ latihan_ifelse.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int angka;
+
+		System.out.println("masukan angka");
+		angka = input.nextInt();
+
+		if (angka % 2 == 0) {
+			System.out.println("Maka angka adalah genap");
+			// untuk mengecek angka prima
+			boolean prima = true;
+			for (int i = 2; i < angka; i++) {
+				if ((angka % i) == 0) {
+					prima = false;
+					break;
+				}
+			}
+			if (prima == true) {
+
+				System.out.println("dan bilangan PRIMA ");
+
+			} else {
+
+				System.out.println("dan bukan bilangan PRIMA ");
+			}
+
+		} else if (angka % 2 != 0) {
+			// untuk mengecek angka prima
+			System.out.println("Maka angka adalah ganjil");
+			
+			boolean prima = true;
+			for (int i = 2; i < angka; i++) {
+				if ((angka % i) == 0) {
+					prima = false;
+					break;
+				}
+			}
+			if (prima == true) {
+
+				System.out.println("dan bilangan PRIMA ");
+
+			} else {
+
+				System.out.println("dan bukan bilangan PRIMA ");
+			}
+
+		}
+	}
+}
